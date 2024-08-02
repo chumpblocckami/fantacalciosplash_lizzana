453b8fb1d520e95dbf9ace9494b4a727b2fcae18
matteo.mazzola <matteo.mazzola@glassform.ai>
2024-08-02 14:23:52 +0200

    feat: added editions

--- src/components/registration_form.py
+++ src/components/registration_form.py
@@ -6,43 +6,43 @@ from src.utils import submit_team, update_budget
 
 
 class RegistrationForm:
-    def __init__(self):
+    def __init__(self, edition):
+        self.edition = edition
+        self.edition_data = st.session_state["giocatori"][self.edition]
         pass
 
     def render(self):
         budget = st.empty()
         budget.write(f"Budget: {st.session_state['budget']}")
 
-        allenatore = st.text_input("Nominativo: ")
+        allenatore = st.text_input("Nominativo: ", key=f"coach_{self.edition}")
 
         portiere = st.multiselect(
             label="Portiere",
             placeholder="Scegli un portiere",
-            options=st.session_state["portieri"],
+            options=self.edition_data.loc[self.edition_data["Ruolo"] == "Portiere"],
             max_selections=1,
+            key=f"portiere_{self.edition}",
         )
 
-        movimento = st.multiselect(
+        titolari = st.multiselect(
             label="Giocatori",
-            options=st.session_state["movimento"],
+            options=self.edition_data.loc[self.edition_data["Ruolo"] == "Movimento"],
             placeholder="Scegli 3 titolari",
             max_selections=3,
+            key=f"movimento_{self.edition}",
         )
 
         riserve = st.multiselect(
             label="Riserve",
             placeholder="Scegli 3 riserve",
-            options=st.session_state["movimento"],
+            options=self.edition_data.loc[self.edition_data["Ruolo"] == "Movimento"],
             max_selections=3,
+            key=f"riserve_{self.edition}",
         )
 
         update_budget(budget)
 
-        st.session_state["allenatore"] = allenatore
-        st.session_state["portiere"] = portiere
-        st.session_state["titolari"] = movimento
-        st.session_state["riserve"] = riserve
-
-        btn_submit = st.button("Iscrivi squadra")
+        btn_submit = st.button("Iscrivi squadra", key=f"iscrizione_{self.edition}")
         if btn_submit:
-            submit_team()
+            submit_team(allenatore, portiere, titolari, riserve, self.edition)
